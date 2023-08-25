021fff0bcea3d27ffdf32d8c3460975f421a96c2
Gyeongjin Lee <asdfg031897@gmail.com>
2023-08-24 20:24:18 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import UserLogin from "./UserLogin/UserLogin";
 import Info from "./CheckArguments/Info";
+import FighterName from "./CheckArguments/FighterName";
 
 function App() {
   return (
     <div>
       <UserLogin />
+      <FighterName />
       <Info />
     </div>
   );
--- src/CheckArguments/FighterName.jsx
+++ src/CheckArguments/FighterName.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function FighterName() {
+  const [name, setName] = useState();
+  const [gym, setGym] = useState();
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const handleChangeGym = (e) => {
+    setGym(e.target.value);
+  };
+
+  //8주차 실습2 계속해보쟝구
   return (
     <form>
-      <fieldset></fieldset>
+      <fieldset>
+        <label htmlFor="name">name</label>
+        <input
+          name="name"
+          id="name"
+          placeholder="Fill in Fighter's name"
+          type="text"
+          className="form-input"
+          onChange={handleChangeName}
+          value={name}
+        />
+        <label htmlFor="name">Gym name</label>
+        <input
+          name="name"
+          id="name"
+          placeholder="Fill in Fighter's Gym name"
+          type="text"
+          onChange={handleChangeGym}
+          className="form-input"
+          value={gym}
+        />
+      </fieldset>
     </form>
   );
 }
