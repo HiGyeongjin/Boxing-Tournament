95e743acaaa1d6e08a507dacc2a287475f201e7f
Gyeongjin Lee <asdfg031897@gmail.com>
2023-08-27 17:12:17 -0700

    initial commit

--- src/CheckArguments/FighterName.jsx
+++ src/CheckArguments/FighterName.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import "./fighter-name.css";
 
+//상태를 정의하기 위한 inputStatus
 const InputStatus = {
   NORMAL: "normal",
   ERROR: "error",
@@ -20,20 +22,59 @@ export default function FighterName() {
     setGym(e.target.value);
   };
 
-  //8주차 실습2 계속해보쟝구
+  const validateName = () => {
+    if (name.length < 1 || name.length > 30) {
+      setNameInputStatus(InputStatus.ERROR);
+      return;
+    }
+    setNameInputStatus(InputStatus.SUCCESS);
+  };
+
+  const validateGymName = () => {
+    if (gym.length < 1 || gym.length > 30) {
+      setGymInputStatus(InputStatus.ERROR);
+      return;
+    }
+    setGymInputStatus(InputStatus.SUCCESS);
+  };
+
+  const submitForm = () => {
+    console.log("submit form!");
+    validateName(name);
+    validateGymName(gym);
+  };
+
+  const resetForm = () => {
+    setName("");
+    setGym("");
+  };
+
+  const getInputStatusStyle = (status) => {
+    if (status === InputStatus.ERROR) {
+      return "input-invalid";
+    } else if (status === InputStatus.SUCCESS) {
+      return "input-valid";
+    }
+    return "";
+  };
+
   return (
     <form>
       <fieldset>
-        <label htmlFor="name">name</label>
+        <label htmlFor="name">Boxer Name</label>
         <input
           name="name"
           id="name"
           placeholder="Fill in Fighter's name"
           type="text"
-          className="form-input"
           onChange={handleChangeName}
+          className={"form-input " + getInputStatusStyle(nameInputStatus)}
           value={name}
         />
+        <div>
+          {nameInputStatus === InputStatus.ERROR &&
+            "Name has to be more than one character."}
+        </div>
       </fieldset>
       <fieldset>
         <label htmlFor="name">Gym name</label>
@@ -46,11 +87,29 @@ export default function FighterName() {
           className="form-input"
           value={gym}
         />
+        <div>
+          {gymInputStatus === InputStatus.ERROR && "Insert without space."}
+        </div>
       </fieldset>
       <div>
-        <button>Submit</button>
-        <button>Reset</button>
+        <button type="button" onClick={resetForm}>
+          Reset
+        </button>
+        <button type="button" onClick={submitForm}>
+          Submit
+        </button>
       </div>
+
+      {/* debug code */}
+      {/* <pre className="debug">
+        <code>
+          {JSON.stringify(
+            { name, gym, nameInputStatus, gymInputStatus },
+            null,
+            2
+          )}
+        </code>
+      </pre> */}
     </form>
   );
 }
