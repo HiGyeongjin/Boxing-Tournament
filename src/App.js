27e39e55e19a0403282cd8b75d0c98bc7d57e57d
Gyeongjin Lee <asdfg031897@gmail.com>
2023-09-04 21:33:41 -0700

    tournament

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import UserLogin from "./UserLogin/UserLogin";
 import Info from "./CheckArguments/Info";
 import FighterName from "./CheckArguments/FighterName";
+import Tournament from "./Tournament/tournament";
 
 function App() {
   return (
     <div>
       <UserLogin />
       <FighterName />
+      <Tournament />
       <Info />
     </div>
   );
