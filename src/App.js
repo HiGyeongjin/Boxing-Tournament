c3ff73f32a279eaf0d171bed191b39f2de3933f0
Gyeongjin Lee <asdfg031897@gmail.com>
2023-09-27 19:55:33 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import Info from "./CheckArguments/Info";
 import FighterName from "./CheckArguments/FighterName";
 import Tournament from "./Tournament/tournament";
 // import axios from "axios";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Link } from "react-router-dom";
+
+//figure out how to make router inside of router.
+//i have router in UserLogin. and index.js has router too.
 
 function App() {
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<UserLogin />} />
-        <Route path="/tournament" element={<Tournament />} />
-        <Route path="/figtername" element={<FighterName />} />
-        <Route path="/info" element={<Info />} />
+        <Link path="/" element={<UserLogin />} />
+        <Link path="/tournament" element={<Tournament />} />
+        <Link path="/figtername" element={<FighterName />} />
+        <Link path="/info" element={<Info />} />
       </Routes>
     </div>
   );
