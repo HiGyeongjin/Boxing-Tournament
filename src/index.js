ee9ecb64cec6121a7aa40b33b11f4ac444dc6137
Gyeongjin Lee <asdfg031897@gmail.com>
2023-10-12 18:50:18 -0700

    screen appear

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<App />} />
-//       </Routes>
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
