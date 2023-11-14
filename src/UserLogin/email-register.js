62349ad891e36768462c2df5b8fb564f509827f8
Gyeongjin Lee <asdfg031897@gmail.com>
2023-11-13 19:56:43 -0800

    modifing register page...

--- src/UserLogin/email-register.js
+++ src/UserLogin/email-register.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function EmailRegister() {
-  return <div>Register page</div>;
+  return (
+    <>
+      <h1>Register page</h1>
+      <input placeholder="email" />
+      <input placeholder="name" />
+      <input placeholder="gym" />
+
+      <button>register</button>
+      <button>reset</button>
+    </>
+  );
 }
