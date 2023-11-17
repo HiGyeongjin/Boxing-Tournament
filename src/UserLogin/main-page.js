d88d724e0a0efcc561e69f7e6a1aa591a1f3ecf1
Gyeongjin Lee <asdfg031897@gmail.com>
2023-11-16 20:03:26 -0800

    working on header..

--- src/UserLogin/main-page.js
+++ src/UserLogin/main-page.js
@@ -7,7 +7,7 @@ export default function MainPage() {
       <img
         src="/b-glove-image.png"
         alt="boxing glove"
-        style={{ width: "100px", margin: "15px" }}
+        style={{ width: "100px", margin: "15px", height: "150px" }}
       />
       <h1>This is a Main page.</h1>
       <NavStyle>
@@ -25,15 +25,6 @@ export default function MainPage() {
         the left of the page.
       </div>
       <Link to="/login">Login</Link>
-
-      <Navbar>
-        <NavButton>Menu</NavButton>
-        <DropdownContent>
-          <DropdownLink href="#home">Home</DropdownLink>
-          <DropdownLink href="#contact">Contact</DropdownLink>
-          <DropdownLink href="#about">About</DropdownLink>
-        </DropdownContent>
-      </Navbar>
     </>
   );
 }
@@ -62,50 +53,4 @@ const DivStyle = styled.div`
 const PStyle = styled.p`
   border: 1px solid pink;
   z-index: 3;
-`;
-
-// Styled components
-const Navbar = styled.div`
-  overflow: hidden;
-  background-color: #333;
-`;
-
-const NavButton = styled.button`
-  float: left;
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  background-color: inherit;
-  font-family: inherit;
-  border: none;
-  outline: none;
-  margin: 0;
-
-  &:hover {
-    background-color: #ddd;
-    color: black;
-  }
-`;
-
-const DropdownContent = styled.div`
-  display: none;
-  position: absolute;
-  background-color: blue;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-  z-index: 1;
-`;
-
-const DropdownLink = styled.a`
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-
-  &:hover {
-    background-color: #ddd;
-    color: pink;
-  }
 `;
--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -3,11 +3,48 @@ import styled from "styled-components";
 
 export default function Header() {
   return (
-    <div>
-      <NavStyle>
+    <NavStyle>
+      <Wrapper>
         <H1Style>
-          <ImageStyle src="https://boxingbc.ca/wp-content/uploads/2022/08/boxing-british-columbia-logo-black.png" />
+          <ImageStyle
+            src="https://boxingbc.ca/wp-content/uploads/2022/08/boxing-british-columbia-logo-black.png"
+            alt="main-logo"
+          />
         </H1Style>
+
+        <NavLinks>
+          <ListStyle>
+            <LinkStyle to="/">Home</LinkStyle>
+          </ListStyle>
+          <ListStyle>
+            <LinkStyle href="#">Tournament</LinkStyle>
+          </ListStyle>
+          <ListStyle>
+            <LinkStyle href="#">Account</LinkStyle>
+            {/* onMouseEnter={} 이거 바로 위에 useState넣어서 만들어보기*/}
+            <DropMenuStyle>
+              <ListStyle>
+                <DropdownStyle to="/login">Login</DropdownStyle>
+              </ListStyle>
+              <ListStyle>
+                <DropdownStyle to="/">Drop menu 2</DropdownStyle>
+              </ListStyle>
+              <ListStyle>
+                <DropdownStyle to="/">Drop menu 3</DropdownStyle>
+              </ListStyle>
+              <ListStyle>
+                <DropdownStyle to="/">Drop menu 4</DropdownStyle>
+              </ListStyle>
+            </DropMenuStyle>
+          </ListStyle>
+          <ListStyle>
+            <LinkStyle href="#">Gym list</LinkStyle>
+          </ListStyle>
+          <ListStyle>
+            <LinkStyle to="/login">Login</LinkStyle>
+          </ListStyle>
+        </NavLinks>
+        {/* 
         <Ulstyled>
           <Listyled>
             <LinkStyle to="/">Home</LinkStyle>
@@ -29,39 +66,88 @@ export default function Header() {
           <Listyled>
             <LinkStyle to="/fighter-name">Fighter name</LinkStyle>
           </Listyled>
-        </Ulstyled>
-      </NavStyle>
-    </div>
+        </Ulstyled> */}
+      </Wrapper>
+    </NavStyle>
   );
 }
 
 const NavStyle = styled.nav`
+  background-color: pink;
+  position: fixed;
+  width: 100%;
+  z-index: 999;
+  height: 80px;
+`;
+
+const Wrapper = styled.nav`
+  max-width: 1250px;
+  padding: 0 30px;
+  margin: auto;
   display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
-const H1Style = styled.h1`
-  flex: 1;
-`;
+const H1Style = styled.h1``;
 
 const ImageStyle = styled.img`
   width: 300px;
 `;
-const Ulstyled = styled.ul`
-  list-style-type: none;
-  display: flex;
-  justify-content: space-around;
-  flex: 2;
-`;
 
-const Listyled = styled.li`
-  border: 1px solid red;
-  margin: 50px 10px;
-  border-radius: 15px;
-  flex: 1;
-  text-align: center;
+const NavLinks = styled.ul`
+  display: inline-flex;
+`;
+const DropMenuStyle = styled.ul`
+  background-color: #242526;
+  width: 150px;
+  top: 65px;
+  line-height: 45px;
+  position: absolute;
 `;
 
 const LinkStyle = styled(Link)`
   text-decoration: none;
-  font-weight: bold;
-  color: black;
+  border-radius: 5px;
+  color: white;
+  padding: 9px 15px;
+  translition: all 0.3s ease;
+  &:hover {
+    background-color: #3a3b3c;
+  }
 `;
+
+const DropdownStyle = styled(Link)`
+  text-decoration: none;
+  color: white;
+  padding: 9px 15px;
+  translition: all 0.3s ease;
+  width: 100%;
+  display: block;
+  padding: 0 0 0 15px;
+  font-weight: 400px;
+  border-radius: 0px;
+  &:hover {
+    background-color: #3a3b3c;
+    color: pink;
+  }
+`;
+
+const ListStyle = styled.li`
+  list-style: none;
+`;
+
+//내가 만든거
+// const Ulstyled = styled.ul`
+//   list-style-type: none;
+//   display: flex;
+//   justify-content: space-around;
+//   flex: 2;
+// `;
+
+// const Listyled = styled.li`
+//   border: 1px solid red;
+//   margin: 50px 10px;
+//   border-radius: 15px;
+//   flex: 1;
+//   text-align: center;
+// `;
